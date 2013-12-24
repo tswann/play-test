00e09b7fe5ff8ec27e13431246a6f41226701a3c
Thomas Swann <ts.swann@gmail.com>
2013-12-24 14:33:01 +0000

    Inital commit

---
+++ app/controllers/Contacts.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Contacts  extends Controller {
+	
+	public static Result index() {
+		return TODO;
+	}
+
+	public static Result create() {
+		return TODO;
+	}
+
+	public static Result edit(Long id) {
+		return TODO;
+	}
+
+	public static Result update(Long id) {
+		return TODO;
+	}
+
+	public static Result delete(Long id) {
+		return TODO;
+	}
+}
---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "Contacts"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  javaEbean,
+  cache
+)     
+
+play.Project.playJavaSettings
